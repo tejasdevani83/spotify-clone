7dc085be5e725085085b5764e472abe9b463eae6
tejasdevani83 <devanitejas23@gmail.com>
2020-12-09 19:18:07 +0530

    Intial setup

---
+++ src/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./login.css";
+import { loginUrl } from "./Spotify";
+
+export default function Login() {
+  return (
+    <div className="login">
+      <img
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+      <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
+    </div>
+  );
+}
---
+++ src/Spotify.js
@@ -0,0 +1,25 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+const redirectUri = "http://localhost:3000/";
+const clientId = "b1407a5bb61044ca9da088aaa6db8041";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+export const loginUrl = `${authEndpoint}/?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
+
+export const getToken = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
